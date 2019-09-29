41d3a90305a990284bbc7c71adf2112539c5b356
Alex <alex@too.gy>
2019-09-29 23:06:30 +0200

    style: Make parameters more readable

--- agent.py
+++ agent.py
@@ -8,11 +8,11 @@ import torch
 import torch.nn.functional as F
 import torch.optim as optim
 
-BUFFER_SIZE = int(1e5)  # replay buffer size
+BUFFER_SIZE = 10000     # replay buffer size
 BATCH_SIZE = 64         # minibatch size
 GAMMA = 0.99            # discount factor
-TAU = 1e-3              # for soft update of target parameters
-LR = 5e-4               # learning rate 
+TAU = 0.001             # for soft update of target parameters
+LR = 0.0005             # learning rate 
 UPDATE_EVERY = 4        # how often to update the network
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
